3364a1564cc7cd1fe09611ea2423b3db35754ed5
Vyacheslav Chernyshoff <slava_rossii@list.ru>
2022-11-13 12:03:30 +0300

    + role test

--- src/test/java/ru/xpendence/topjavagraduation/service/impl/RoleServiceTest.java
+++ src/test/java/ru/xpendence/topjavagraduation/service/impl/RoleServiceTest.java
@@ -5,12 +5,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import ru.xpendence.topjavagraduation.AbstractTest;
 import ru.xpendence.topjavagraduation.service.RoleService;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class RoleServiceTest extends AbstractTest {
 
     @Autowired
     private RoleService service;
+
+    @Test
+    void getById() {
+        assertDoesNotThrow(() -> service.getById(1L));
+    }
 
     @Test
     void getAll() {
